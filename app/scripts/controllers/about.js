9c8c8c248cb394d9d74e78d824ec676838f59224
carlos <carlosthe19916@gmail.com>
2015-11-30 10:28:01 -0500

    yeoman initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name repeidApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the repeidApp
+ */
+angular.module('repeidApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name repeidApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the repeidApp
+ */
+angular.module('repeidApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
